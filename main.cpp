809510c80fa09d41ac000d15949a492d0a49a107
Bajuuukeed <151462003+Bajuuukeed@users.noreply.github.com>
2026-04-01 19:31:29 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 SISTEM DE GESTIUNE A UNEI SALI DE SPORT. ACEST SISTEM INCLUDE ADAUGAREA, REACTIVAREA , CAUTAREA ABONAMENTELOR CLIENTIILOR,
 IAR PENTRU CONCRETIZAREA VALIDITATII ACESTUI PROIECT AM INCLUS SI O FUNCTIE DE ACCELERARE A TIMPULUI PENTRU A VEDEA CUM
 ABONAMENTELE POT EXPIRA. EXISTA 2 TABELE, 1 PENTRU CLIENTII A CAROR ABONAMENTE SUNT ACTIVE SI 1 PENTRU TOATE ABONAMENTELE
- CE AU EXISTAT VREODATA. DATELE SUNT ADAUGATE DE LA TASTATURA, PROGRAMUL AVAND O INTERFATA DEZVOLTATATA SPECIAL PENTRU
- EFICIENTIZAREA BAZEI DE DATE A UNEI SALI ;].
+CE AU EXISTAT VREODATA. DATELE SUNT ADAUGATE DE LA TASTATURA, PROGRAMUL AVAND O INTERFATA DEZVOLTATATA SPECIAL PENTRU
+EFICIENTIZAREA BAZEI DE DATE A UNEI SALI ;].
  */
 #include <iostream>
 #include <fstream>
@@ -26,27 +26,32 @@ public:
     int getZi() const {
         return zi;
     }
+
     int getLuna() const {
         return luna;
     }
     friend std::ostream& operator<<(std::ostream& os, const Data& d) {
-        if (d.zi < 10) os << "0";
+        if (d.zi < 10)
+            os << "0";
         os << d.zi << "/";
-        if (d.luna < 10) os << "0";
+        if (d.luna < 10)
+            os << "0";
         os << d.luna;
         return os;
     }
 };
+
 class Abonament {
     char denumire[30];
     int durataInitiala;
+    float pret;
+    bool esteReactivare;
 public:
     Abonament() {
         durataInitiala = 30;
-        const char* def = "Standard";
-        int j = 0;
-        while(def[j] != '\0') { denumire[j] = def[j]; j++; }
-        denumire[j] = '\0';
+        pret = 0;
+        esteReactivare = false;
+        denumire[0] = '\0';
     }
     void setTip(const char* nume) {
         int j = 0;
@@ -55,29 +60,56 @@ public:
             j++;
         }
         denumire[j] = '\0';
+        if (strcmp(denumire, "SpartanPlatinum") == 0) {
+            pret = 220;
+        } else if (strcmp(denumire, "Spartan") == 0) {
+            pret = 150;
+        } else {
+            pret = 100;
+        }
     }
+    void marcheazaReactivare() {
+        esteReactivare = true;
+        pret = pret * 0.90;
+    }
+    float getPret() const {
+        return pret;
+    }
+    bool eReactivare() const {
+        return esteReactivare;
+    }
+
     int calculeazaZileRamase(int dif) const {
         int ramase = durataInitiala - dif;
         if (ramase < 0) return 0;
         return ramase;
     }
     friend std::ostream& operator<<(std::ostream& os, const Abonament& a) {
-        os << "Tip: " << a.denumire;
+        os << "Tip: " << a.denumire << " | Pret: " << a.pret << " RON";
+        if (a.esteReactivare) os << " (reducere 10% aplicata)";
         return os;
     }
+    static int strcmp(const char* a, const char* b) {
+        while (*a && (*a == *b)) { a++; b++; }
+        return (unsigned char)*a - (unsigned char)*b;
+    }
 };
+
 class Client {
     char nume[50];
     int id;
     Data dataInceput;
     Abonament tipAb;
+    int nrReactivari;
 public:
     Client() {
         id = 0;
+        nrReactivari = 0;
         nume[0] = '\0';
     }
     Client(const Client& altul) {
         id = altul.id;
+        nrReactivari = altul.nrReactivari;
         dataInceput = altul.dataInceput;
         tipAb = altul.tipAb;
         int j = 0;
@@ -86,9 +118,11 @@ public:
         }
         nume[j] = '\0';
     }
+
     Client& operator=(const Client& altul) {
         if (this != &altul) {
             id = altul.id;
+            nrReactivari = altul.nrReactivari;
             dataInceput = altul.dataInceput;
             tipAb = altul.tipAb;
             int j = 0;
@@ -102,9 +136,17 @@ public:
     ~Client() {}
     void reactivare(int z, int l) {
         dataInceput.setData(z, l);
+        tipAb.marcheazaReactivare(); // aplica reducerea
+        nrReactivari++;
+        std::cout << "=================================" << std::endl;
+        std::cout << "  ABONAMENT REACTIVAT CU SUCCES!" << std::endl;
+        std::cout << "  Pret de plata: " << tipAb.getPret() << " RON";
+        std::cout << " (reducere 10% pentru client fidel)" << std::endl;
+        std::cout << "=================================" << std::endl;
     }
     void citesteClient(const char* n, int i, int z, int l, const char* numeAb) {
         id = i;
+        nrReactivari = 0;
         dataInceput.setData(z, l);
         tipAb.setTip(numeAb);
         int j = 0;
@@ -120,12 +162,20 @@ public:
         int diferenta = totalZileAcum - totalZileStart;
         return tipAb.calculeazaZileRamase(diferenta);
     }
+
+    float getPretAbonament() const {
+        return tipAb.getPret();
+    }
+
     friend std::ostream& operator<<(std::ostream& os, const Client& c) {
         os << "ID: " << c.id << " | Nume: " << c.nume << " | " << c.tipAb
            << " | Data activare: " << c.dataInceput;
+        if (c.nrReactivari > 0)
+            os << " | Reactivari: " << c.nrReactivari;
         return os;
     }
 };
+
 class SalaSport {
     Client* bazaDate;
     int nr;
@@ -140,22 +190,37 @@ public:
     ~SalaSport() {
         delete[] bazaDate;
     }
-
     void afiseazaRegulament() const {
         std::ifstream f("Regulament.txt");
         char linie[200];
-            while (f.getline(linie, 200)) {
-                std::cout << linie << "\n";
-            }
-            f.close();
+        while (f.getline(linie, 200)) {
+            std::cout << linie << std::endl;
+        }
+        f.close();
     }
+
+
     void afiseazaBeneficii() const {
         std::ifstream f("Beneficii.txt");
         char linie[200];
-            while (f.getline(linie, 200)) {
-                std::cout << linie << "\n";
-            }
-            f.close();
+        while (f.getline(linie, 200)) {
+            std::cout << linie << std::endl;
+        }
+        f.close();
+
+        std::cout << std::endl;
+        std::cout << "=========================================" << std::endl;
+        std::cout << "          LISTA PRETURI ABONAMENTE       " << std::endl;
+        std::cout << "=========================================" << std::endl;
+        std::cout << "  1. Luptator      ->  100 RON / luna    " << std::endl;
+        std::cout << "-----------------------------------------" << std::endl;
+        std::cout << "  2. Spartan       ->  150 RON / luna    " << std::endl;
+        std::cout << "-----------------------------------------" << std::endl;
+        std::cout << "  3. SpartanPlatinum -> 220 RON / luna   " << std::endl;
+        std::cout << "-----------------------------------------" << std::endl;
+        std::cout << "  * Reactivare: -10% reducere fideli!    " << std::endl;
+        std::cout << "=========================================" << std::endl;
+        std::cout << std::endl;
     }
 
     void activareID() {
@@ -164,46 +229,52 @@ public:
         std::cin >> idCautat;
         if (idCautat > 0 && idCautat <= nr) {
             bazaDate[idCautat - 1].reactivare(ziSistem, lunaSistem);
-            std::cout << "Abonament reactivat!";
-            std::cout << std::endl;
         } else {
-            std::cout << "Eroare: Id-ul pus nu exista.";
-            std::cout << std::endl;
+            std::cout << "Eroare: Id-ul pus nu exista." << std::endl;
             exit(0);
         }
     }
+
     void adauga() {
         if (nr >= 100) return;
         char n[50];
         int optAb;
-        std::cout << "Cum te numesti?";
+        std::cout << "Cum te numesti? ";
         std::cin.get();
-        std::cin.getline(n,50);
+        std::cin.getline(n, 50);
         afiseazaBeneficii();
-        std::cout << "Alegeti tip(1.Platinum, 2.Spartan,3.Luptator): ";
+        std::cout << "Alegeti tip (1.SpartanPlatinum, 2.Spartan, 3.Luptator): ";
         std::cin >> optAb;
         const char* t;
-        if (optAb == 1) t = "SpartanPlatinum";
-        else if (optAb == 2) t = "Spartan";
-        else t = "Luptator";
+        float pretAfisat;
+        if (optAb == 1) { t = "SpartanPlatinum"; pretAfisat = 220; }
+        else if (optAb == 2) { t = "Spartan"; pretAfisat = 150; }
+        else { t = "Luptator"; pretAfisat = 100; }
         bazaDate[nr].citesteClient(n, nr + 1, ziSistem, lunaSistem, t);
         nr++;
-        std::cout << "Abonamentul " << t << " a fost  activat!";
+        std::cout << std::endl;
+        std::cout << "=========================================" << std::endl;
+        std::cout << "  Abonament " << t << " activat cu succes!" << std::endl;
+        std::cout << "  DE PLATA: " << pretAfisat << " RON" << std::endl;
+        std::cout << "=========================================" << std::endl;
         std::cout << std::endl;
     }
+
     void afisareStatus(const Client& c) const {
         int ramase = c.getZileRamase(ziSistem, lunaSistem);
         std::cout << c;
-        if (ramase > 0) std::cout << " | STATUS: ACTIV (" << ramase << " zile)\n";
-        else std::cout << " | STATUS: EXPIRAT\n";
+        if (ramase > 0) std::cout << " | STATUS: ACTIV (" << ramase << " zile)" << std::endl;
+        else std::cout << " | STATUS: EXPIRAT" << std::endl;
     }
+
     void cautareID() const {
         int idCautat;
         std::cout << "Id client: ";
         std::cin >> idCautat;
         if (idCautat > 0 && idCautat <= nr) afisareStatus(bazaDate[idCautat - 1]);
-        else std::cout << "Id-ul nu exista.\n";
+        else std::cout << "Id-ul nu exista." << std::endl;
     }
+
     void simuleazaTimp() {
         int zile;
         std::cout << "Cate zile vrei sa treaca? ";
@@ -213,53 +284,44 @@ public:
             ziSistem -= 30;
             lunaSistem++;
         }
-        std::cout << "Data noua:" << ziSistem << "/" << lunaSistem << std::endl;
+        std::cout << "Data noua: " << ziSistem << "/" << lunaSistem << std::endl;
     }
+
     void afisareActivi() const {
         std::cout << std::endl;
-        std::cout << "Abonamente active";
-        std::cout << std::endl;
+        std::cout << "--- Abonamente active ---" << std::endl;
         for (int i = 0; i < nr; i++) {
             if (bazaDate[i].getZileRamase(ziSistem, lunaSistem) > 0)
                 afisareStatus(bazaDate[i]);
         }
     }
+
     void istoric() const {
         std::cout << std::endl;
-        std::cout << "Istoric Clienti";
-        std::cout << std::endl;
+        std::cout << "--- Istoric Clienti ---" << std::endl;
         for (int i = 0; i < nr; i++)
             afisareStatus(bazaDate[i]);
     }
 };
+
 int main() {
     SalaSport sala;
     sala.afiseazaRegulament();
     int opt, sub;
     do {
-        std::cout << "1.GESTIUNE";
-        std::cout << std::endl;
-        std::cout << "2.TIMP";
-        std::cout << std::endl;
-        std::cout << "3.PANOU BENEFICII";
-        std::cout << std::endl;
-        std::cout << "0.IESIRE";
-        std::cout << std::endl;
+        std::cout << "1.GESTIUNE" << std::endl;
+        std::cout << "2.TIMP" << std::endl;
+        std::cout << "3.PANOU BENEFICII & PRETURI" << std::endl;
+        std::cout << "0.IESIRE" << std::endl;
         std::cin >> opt;
         if (opt == 1) {
             do {
-                std::cout << "1.Adauga";
-                std::cout << std::endl;
-                std::cout << "2.Cautare";
-                std::cout << std::endl;
-                std::cout << "3.Activi";
-                std::cout << std::endl;
-                std::cout << "4.Tot ";
-                std::cout << std::endl;
-                std::cout << "5.Reactivare Abonament";
-                std::cout << std::endl;
-                std::cout << "6.Inapoi";
-                std::cout << std::endl;
+                std::cout << "1.Adauga" << std::endl;
+                std::cout << "2.Cautare" << std::endl;
+                std::cout << "3.Activi" << std::endl;
+                std::cout << "4.Tot" << std::endl;
+                std::cout << "5.Reactivare Abonament" << std::endl;
+                std::cout << "6.Inapoi" << std::endl;
                 std::cin >> sub;
                 if (sub == 1) sala.adauga();
                 else if (sub == 2) sala.cautareID();
@@ -273,5 +335,6 @@ int main() {
             sala.afiseazaBeneficii();
         }
     } while (opt);
+
     return 0;
 }
